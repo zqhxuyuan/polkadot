260c2cafc838aee666b9e4293924d2fff243ebac
zqh <zqhxuyuan@gmail.com>
2021-12-08 00:50:03 +0800

    add location test

--- xcm/xcm-builder/src/location_conversion.rs
+++ xcm/xcm-builder/src/location_conversion.rs
@@ -200,7 +200,7 @@ mod tests {
 	use super::*;
 
 	use frame_support::parameter_types;
-	use xcm::latest::{Junction, NetworkId::Any};
+	use xcm::latest::{Junction, MultiAsset, NetworkId::Any};
 
 	fn account20() -> Junction {
 		AccountKey20 { network: Any, key: Default::default() }
@@ -272,5 +272,266 @@ mod tests {
 		let input = MultiLocation { parents: 99, interior: X1(Parachain(88)) };
 		let inverted = LocationInverter::<Ancestry>::invert_location(&input);
 		assert_eq!(inverted, Err(()));
+	}
+
+	#[test]
+	fn test1() {
+		// let ancestry: MultiLocation = (Parachain(1000), PalletInstance(42)).into();
+		// 		let target = (Parent, PalletInstance(69)).into();
+		// 		let expected = (Parent, PalletInstance(42)).into();
+		// 		let inverted = ancestry.inverted(&target).unwrap();
+		// 		assert_eq!(inverted, expected);
+	}
+
+	#[test]
+	fn test_invert_take_first() {
+		let mut ancestry: MultiLocation = (Parachain(1000), PalletInstance(42)).into();
+		assert_eq!(ancestry.interior, X2(Parachain(1000), PalletInstance(42)));
+		assert_eq!(2, ancestry.interior.len());
+
+		let first = ancestry.take_first_interior();
+		assert_eq!(first.unwrap(), Parachain(1000));
+		assert_eq!(ancestry, (0, PalletInstance(42)).into());
+
+		let first = ancestry.take_first_interior();
+		assert_eq!(first.unwrap(), PalletInstance(42));
+		assert_eq!(ancestry, (0, Here).into());
+
+		let first = ancestry.take_first_interior();
+		assert_eq!(first, None);
+		assert_eq!(ancestry, (0, Here).into());
+
+		let first = ancestry.take_first_interior();
+		assert_eq!(first, None);
+		assert_eq!(ancestry, (0, Here).into());
+
+		let location: MultiLocation = (2, Here).into();
+		assert_eq!(0, location.interior.len());
+		let location: MultiLocation = (1, Here).into();
+		assert_eq!(0, location.interior.len());
+	}
+
+	#[test]
+	fn test_invert_para_pallet() {
+		parameter_types! {
+			pub Ancestry: MultiLocation = (Parachain(1000), PalletInstance(42)).into();
+		}
+
+		let location: MultiLocation = (0, Here).into();
+		let invert = LocationInverter::<Ancestry>::invert_location(&location).unwrap();
+		assert_eq!(0, invert.parents);
+		assert_eq!(Here, invert.interior);
+
+		let location: MultiLocation = (1, Here).into();
+		let invert = LocationInverter::<Ancestry>::invert_location(&location).unwrap();
+		assert_eq!(0, invert.parents);
+		assert_eq!(X1(Parachain(1000)), invert.interior);
+
+		let location: MultiLocation = (2, Here).into();
+		let invert = LocationInverter::<Ancestry>::invert_location(&location).unwrap();
+		assert_eq!(0, invert.parents);
+		assert_eq!(X2(Parachain(1000), PalletInstance(42)), invert.interior);
+
+		let location: MultiLocation = (3, Here).into();
+		let invert = LocationInverter::<Ancestry>::invert_location(&location).unwrap();
+		assert_eq!(0, invert.parents);
+		assert_eq!(X3(Parachain(1000), PalletInstance(42), OnlyChild), invert.interior);
+	}
+
+	#[test]
+	fn test_invert_para() {
+		parameter_types! {
+			pub Ancestry: MultiLocation = (Parachain(1000)).into();
+		}
+
+		let location: MultiLocation = (1, Here).into();
+		let invert = LocationInverter::<Ancestry>::invert_location(&location).unwrap();
+		assert_eq!(0, invert.parents);
+		assert_eq!(X1(Parachain(1000)), invert.interior);
+
+		let location: MultiLocation = (2, Here).into();
+		let invert = LocationInverter::<Ancestry>::invert_location(&location).unwrap();
+		assert_eq!(0, invert.parents);
+		assert_eq!(X2(Parachain(1000), OnlyChild), invert.interior);
+
+		let location: MultiLocation = (3, Here).into();
+		let invert = LocationInverter::<Ancestry>::invert_location(&location).unwrap();
+		assert_eq!(0, invert.parents);
+		assert_eq!(X3(Parachain(1000), OnlyChild, OnlyChild), invert.interior);
+	}
+
+	#[test]
+	fn test_invert_location_cases() {
+		// Ancestry: root(Relay) -> source(Pallet42): (Parachain(1000), PalletInstance(42))
+		// source->target: Parent, Pallet(69)
+		// target->source: ?
+		//
+		//                    v Source
+		// Relay -> Para 1 -> Pallet(42)
+		//            |
+		//            |-----> Pallet(69)
+		//                    ^ Target
+		parameter_types! {
+			pub Ancestry: MultiLocation = (Parachain(1000), PalletInstance(42)).into();
+		}
+		let target = (Parent, PalletInstance(69)).into();
+		let inverted = LocationInverter::<Ancestry>::invert_location(&target).unwrap();
+		assert_eq!(inverted.parents, 1);
+		assert_eq!(inverted.interior, X1(Parachain(1000)));
+
+		// Ancestry: root(Relay) -> source(Pallet42): (Parachain(1000), PalletInstance(42))
+		// source->target: 2, Pallet(69)
+		// target->source: ?
+		//
+		//                    v Source
+		// Relay -> Para 1 -> Pallet(42)
+		//   |
+		//   |-----> Pallet(69)
+		//           ^ Target
+		let target = (2, PalletInstance(69)).into();
+		let inverted = LocationInverter::<Ancestry>::invert_location(&target).unwrap();
+		assert_eq!(inverted.parents, 1);
+		assert_eq!(inverted.interior, X2(Parachain(1000), PalletInstance(42)));
+
+		//                    v Source
+		// Relay -> Para 1 -> Pallet(42)
+		//       -> Para 2 -> Pallet(43)
+		//                    ^ Target
+		let location: MultiLocation = (2, X2(Parachain(2000), PalletInstance(43))).into();
+		let invert = LocationInverter::<Ancestry>::invert_location(&location).unwrap();
+		assert_eq!(2, invert.parents);
+		assert_eq!(X2(Parachain(1000), PalletInstance(42)), invert.interior);
+
+		//                                  v Source
+		// Relay -> Para 1 -> Pallet(42) -> Pallet(52)
+		//       -> Para 2 -> Pallet(43)
+		//                    ^ Target
+		parameter_types! {
+			pub Ancestry2: MultiLocation = (Parachain(1000), PalletInstance(42), PalletInstance(52)).into();
+		}
+		let location: MultiLocation = (3, X2(Parachain(2000), PalletInstance(43))).into();
+		let invert = LocationInverter::<Ancestry2>::invert_location(&location).unwrap();
+		assert_eq!(2, invert.parents);
+		assert_eq!(X3(Parachain(1000), PalletInstance(42), PalletInstance(52)), invert.interior);
+
+		//                                  v Source
+		// Relay -> Para 1 -> Pallet(42) -> Pallet(52)
+		//       -> Para 2 -> Pallet(43) -> Pallet(53)
+		//                                  ^ Target
+		let location: MultiLocation = (3, X3(Parachain(2000), PalletInstance(43), PalletInstance(53))).into();
+		let invert = LocationInverter::<Ancestry2>::invert_location(&location).unwrap();
+		assert_eq!(3, invert.parents);
+		assert_eq!(X3(Parachain(1000), PalletInstance(42), PalletInstance(52)), invert.interior);
+
+
+		//                                  v Source
+		// Relay -> Para 1 -> Pallet(42) -> GeneralIndex(1)
+		//                 -> Pallet(69) -> GeneralIndex(2)
+		//                    ^ Target
+		parameter_types! {
+			pub Ancestry3: MultiLocation = (Parachain(1000), PalletInstance(42), GeneralIndex(1)).into();
+		}
+		let location: MultiLocation = (Parent, Parent, PalletInstance(69), GeneralIndex(2)).into();
+		let invert = LocationInverter::<Ancestry3>::invert_location(&location).unwrap();
+		assert_eq!(2, invert.parents);
+		assert_eq!(X2(Parachain(1000), PalletInstance(42)), invert.interior);
+	}
+
+	#[test]
+	fn test_parachain_reanchor_sibling() {
+		use frame_support::parameter_types;
+
+		// para.rs runtime config
+		parameter_types! {
+			pub Ancestry: MultiLocation = X1(Parachain(1)).into();
+		}
+
+		// if the original origin is in parachain, and the destination is an sibling parachain
+		// then we could imaging this is an xcmp message which parachain send to parachain.
+		let dest: MultiLocation = (Parent, Parachain(2)).into();
+
+		// Parachain(1) invert (Parent,Parachain(2)) = (Parent, Parachain(1))
+		let inv_dest = LocationInverter::<Ancestry>::invert_location(&dest).unwrap();
+		assert_eq!(inv_dest, (1, Parachain(1)).into());
+
+		// (Here, 100).reanchor((Parent,Parachain(1))) results ((Parent, Parachain(1)), 100)
+		// this is for the case of A -> [A] -> B
+		// (Here,100) means token A, then in para(2), (1, Para(1)) can express the meaning of token A asset
+		let mut asset: MultiAsset = (Here, 100u128).into();
+		asset.reanchor(&inv_dest);
+		assert_eq!(asset, ((Parent, Parachain(1)), 100).into());
+		assert_eq!(asset, ((1, Parachain(1)), 100).into());
+
+		// (Parent, 100).reanchor((Parent,Parachain(1)))
+		// this is for the case of A -> R -> B. i.e. Karura transfer KSM to Bifrost.
+		// so here (Parent, 100) means 100 KSM in karura, and in bifrost,
+		// it also use (Parent, 100) to express 100 KSM.
+		let mut asset: MultiAsset = (Parent, 100u128).into();
+		asset.reanchor(&inv_dest);
+		assert_eq!(asset, (Parent, 100).into());
+
+		// (Parachain(1), 100).reanchor((Parent,Parachain(1)))
+		// it's meaningless, but here we use here just for the testcase
+		let mut asset: MultiAsset = ((0, Parachain(1)), 100u128).into();
+		asset.reanchor(&inv_dest);
+		assert_eq!(asset, ((1, X2(Parachain(1), Parachain(1))), 100).into());
+
+		// (0, Parachain(1)) is like Parachain(1), so it's also meaningless
+		let mut asset: MultiAsset = (Parachain(1), 100u128).into();
+		asset.reanchor(&inv_dest);
+		assert_eq!(asset, ((1, X2(Parachain(1), Parachain(1))), 100).into());
+
+		// (GeneralIndex(42), 100).reanchor((Parent,Parachain(1)))
+		// the original is GeneralIndex belonging to origin parachain(1)
+		// then in the Para(2) side, it needs first get into Para(1), then get into GeneralIndex
+		let mut asset: MultiAsset = ((0, GeneralIndex(42)), 100u128).into();
+		asset.reanchor(&inv_dest);
+		assert_eq!(asset, ((1, X2(Parachain(1), GeneralIndex(42))), 100).into());
+	}
+
+	#[test]
+	fn test_sibling_reanchor() {
+		use frame_support::parameter_types;
+		parameter_types! {
+			pub Ancestry: MultiLocation = X1(Parachain(2001)).into();
+		}
+		let dest: MultiLocation = (Parent, Parachain(2000)).into();
+		let inv_dest = LocationInverter::<Ancestry>::invert_location(&dest).unwrap();
+		assert_eq!(inv_dest, (1, Parachain(2001)).into());
+
+		// the GeneralIndex belong to Parachain(2001)
+		let mut asset: MultiAsset = ((0, GeneralIndex(42)), 100u128).into();
+		asset.reanchor(&inv_dest);
+		assert_eq!(asset, ((1, X2(Parachain(2001), GeneralIndex(42))), 100).into());
+
+		// the GeneralIndex belong to Parachain(2000)
+		let mut asset: MultiAsset = ((Parent, X2(Parachain(2000), GeneralIndex(42))), 100u128).into();
+		asset.reanchor(&inv_dest);
+		assert_eq!(asset, ((1, X2(Parachain(2000), GeneralIndex(42))), 100).into());
+	}
+
+	#[test]
+	fn test_sibling_reanchor_tokens() {
+		use frame_support::parameter_types;
+		parameter_types! {
+			pub Ancestry: MultiLocation = X1(Parachain(2001)).into();
+		}
+		let dest: MultiLocation = (1, Parachain(2000)).into();
+		let inv_dest = LocationInverter::<Ancestry>::invert_location(&dest).unwrap();
+		assert_eq!(inv_dest, (1, Parachain(2001)).into());
+
+		// the GeneralKey(BNC) belong to Parachain(2001)
+		let mut asset: MultiAsset = ((0, GeneralKey("BNC".as_bytes().to_vec())), 100u128).into();
+		asset.reanchor(&inv_dest);
+		assert_eq!(asset, ((1, X2(Parachain(2001), GeneralKey("BNC".as_bytes().to_vec()))), 100).into());
+
+		// the GeneralKey(KAR) and GeneralKey(KUSD) belong to Parachain(2000)
+		let mut asset: MultiAsset = ((1, X2(Parachain(2000), GeneralKey("KAR".as_bytes().to_vec()))), 100u128).into();
+		asset.reanchor(&inv_dest);
+		assert_eq!(asset, ((1, X2(Parachain(2000), GeneralKey("KAR".as_bytes().to_vec()))), 100).into());
+
+		let mut asset: MultiAsset = ((1, Parachain(2000), GeneralKey("KUSD".as_bytes().to_vec())), 100u128).into();
+		asset.reanchor(&inv_dest);
+		assert_eq!(asset, ((1, Parachain(2000), GeneralKey("KUSD".as_bytes().to_vec())), 100).into());
 	}
 }
